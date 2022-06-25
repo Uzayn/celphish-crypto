fbf0ae346ba72aa4b8855090c06482f517c159c7
Koleosho Huzain <95490624+Uzayn@users.noreply.github.com>
2022-06-25 01:27:54 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'hero': "url('resources/hero.png')"
+            }
+        },
+    },
+    plugins: [],
+}
